5839c0ef9227d1981d59a081e9cca2b67beb16e8
Duff OMelia <duff@omelia.org>
2014-06-10 14:34:40 -0400

    Rely on strong_parameters stuff

--- spec/support/models/widget.rb
+++ spec/support/models/widget.rb
@@ -6,9 +6,6 @@ class Widget
   property :shipped_at, Time
   property :restricted, Boolean, :default => false
 
-  attr_protected :manufactured
-  attr_protected :restricted, :as => :default
-
   many :widget_parts
 end
 

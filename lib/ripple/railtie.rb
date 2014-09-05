c3e40f353f1be38c563f7cbc1225e8620a23a96d
Nathaniel Talbott <nathaniel@talbott.ws>
2014-09-05 14:43:20 -0400

    Strip out (broken) rake tasks

--- lib/ripple/railtie.rb
+++ lib/ripple/railtie.rb
@@ -4,8 +4,5 @@ module Ripple
   # Railtie for automatic initialization of the Ripple framework
   # during Rails initialization.
   class Railtie < Rails::Railtie
-    rake_tasks do
-      load "ripple/railties/ripple.rake"
-    end
   end
 end
